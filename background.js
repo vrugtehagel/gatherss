a45cc94b211fac2213acc49ceb53b1f4f4f42b92
vrugtehagel <vrugtehagel@pm.me>
2025-08-06 14:30:13 +0200

    Fix issues with icons when using multiple windows

--- background.js
+++ background.js
@@ -2,47 +2,53 @@ import {refreshAllPosts} from '/-/posts.js'
 import {getSetting, setSetting} from '/-/settings.js'
 
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-	if(tab.active) checkForRssFeed(tab.id)
+	if(tab.active) checkForRssFeed(tab)
 }, {properties: ['url', 'status']})
 
 browser.tabs.onActivated.addListener(({tabId}) => {
-	checkForRssFeed(tabId)
+	browser.tabs.get(tabId).then(checkForRssFeed).catch(() => null)
 })
-
-browser.alarms.create({periodInMinutes: 30})
-browser.alarms.onAlarm.addListener(refresh)
 
 browser.runtime.onMessage.addListener(message => {
 	if(message.type != 'rsscheck') return
-	const querying = browser.tabs.query({active: true, currentWindow: true})
-	querying.then(([tab]) => checkForRssFeed(tab.id))
+	const querying = browser.tabs.query({active: true})
+	querying.then(tabs => tabs.forEach(tab => checkForRssFeed(tab)))
 })
 
-async function checkForRssFeed(tabId){
+const icons = {
+	default: '/icons/rss.svg',
+	available: '/icons/rss-available.svg',
+	unread: '/icons/rss-unread.svg',
+}
+
+async function checkForRssFeed(tab){
 	const unread = await getSetting('unread')
 	if(unread) return
 	const message = {type: 'feeddetect'}
-	const responding = browser.tabs.sendMessage(tabId, message)
+	const responding = browser.tabs.sendMessage(tab.id, message)
 	const response = await responding.catch(() => null)
 	const {feedUrl} = response ?? {}
-	if(!feedUrl) return browser.action.setIcon({path: '/icons/rss.svg'})
+	const windowId = tab.windowId
+	console.log(windowId)
+	if(!feedUrl) return browser.action.setIcon({path: icons.default, windowId})
 	const storage = await browser.storage.sync.get([feedUrl])
 	const discoveryDot = await getSetting('discoveryDot')
 	const showDot = !(feedUrl in storage) && discoveryDot
-	const path = showDot ? '/icons/rss-available.svg' : '/icons/rss.svg'
-	browser.action.setIcon({path})
+	const path = showDot ? icons.available : icons.default
+	browser.action.setIcon({path, windowId})
 }
 
-async function refresh(){
-	const delay = new Promise(resolve => setTimeout(resolve, 1000))
-	browser.action.setIcon({path: '/icons/sync.svg'})
+browser.alarms.create({periodInMinutes: .1})
+browser.alarms.onAlarm.addListener(async () => {
 	const unreadDot = await getSetting('unreadDot')
-	if(!unreadDot) return browser.action.setIcon({path: '/icons/rss.svg'})
+	if(!unreadDot) return
 	const unreadSetting = await getSetting('unread')
 	const newUnread = await refreshAllPosts()
 	if(!unreadSetting && newUnread) await setSetting('unread', true)
 	const unread = unreadSetting || newUnread
-	const path = unread ? '/icons/rss-unread.svg' : '/icons/rss.svg'
-	await delay
-	browser.action.setIcon({path})
-}
+	if(!unread) return
+	const path = icons.unread
+	const windows = await browser.windows.getAll()
+	const windowIds = windows.map(({id}) => id)
+	windowIds.forEach(windowId => browser.action.setIcon({path, windowId}))
+})
